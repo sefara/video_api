68d61d654a77e116da98afde60cfa0ef034105c5
User <passive@ip-172-31-18-253.eu-west-1.compute.internal>
2024-05-31 12:30:19 +0000

    fix after deployment - spacing and default variable setup in youtube client

--- video_api.py
+++ video_api.py
@@ -3,42 +3,42 @@ from youtube_client import youtube_client
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def test1():
     print("FLASK TEST RUN OK")
     return ('Welcom to the PASSIVE INCOME API')
 
-@app.route('/api/v1/integration/test', methods=['GET','POST'])
-def test2():
-	data = request.get_json()
-	req_id=data['record']['id']
-    
-	text_to_return='TEST OK: Integration test - Value of Video ID: '+ str(req_id) + 'my data:' +data
-	print(text_to_return)
-	return (text_to_return)
 
-@app.route('/api/v1/youtube/publish', methods=['GET','POST'])
+@app.route('/api/v1/integration/test', methods=['GET', 'POST'])
+def test2():
+    data = request.get_json()
+    req_id = data['record']['id']
+    text_to_return = 'TEST OK: Integration test - Value of Video ID: ' + \
+        str(req_id) + 'my data:' + data
+    print(text_to_return)
+    return (text_to_return)
+
+
+@app.route('/api/v1/youtube/publish', methods=['GET', 'POST'])
 def youtube_publish():
     print('TODO: UPRAV MA ABY SOM VZDY BOLA DEFAULT NULL /// IBA ZE BY BOL DEBUG A VTEDY SI NASTAVIM HODNOTU O RIADOK NIZSIE')
     data = jsonify('{"my_sample:test", "record:"{"id=1","channel_id=1"}"}')
-
     if request is not None:
-        data = request.get_json()    
-    print (data)
-    
-    if data is None or 'record' not in data or data['record'] is None or 'id' not in data['record'] or data['record']['id'] is None or 'channel_id' not in data['record'] or data['record']['channel_id'] is None :
-         return 'Wrong request'
+        data = request.get_json()
+    print(data)
+    if data is None or 'record' not in data or data['record'] is None or 'id' not in data['record'] or data['record']['id'] is None or 'channel_id' not in data['record'] or data['record']['channel_id'] is None:
+        return 'Wrong request'
     channel_id = data['record']['channel_id']
-    print (channel_id)
+    print(channel_id)
     video_data = data['record']
-    
-    my_client = youtube_client(channel_id)        
+    my_client = youtube_client(channel_id)
     video_processing_result = my_client.youtube_upload(video_data)
-    video_processing_result = my_client.update_video_status(video_data, video_processing_result)
-    return 'Youtube publisher function finished'  
+    video_processing_result = my_client.update_video_status(
+        video_data, video_processing_result)
+    return 'Youtube publisher function finished'
+
 
 # main driver function
 if __name__ == '__main__':
-    # run() method of Flask class runs the application
-    # on the local development server.
     app.run()
--- youtube_client.py
+++ youtube_client.py
@@ -7,9 +7,9 @@ import os
 
 
 class youtube_client:
-    filecloud_dir
-    video_dir
-    channel_data
+    filecloud_dir = "/home/passive/filecloud/"
+    video_dir = "focus10x6/"
+    channel_data = "{}"
     VALID_PRIVACY_STATUSES = ["public", "private", "unlisted"]
     VALID_VIDEO_CATEGORIES = [21, 22, 23]
 
@@ -23,7 +23,8 @@ class youtube_client:
         supa_api: RestClient = RestClient(url, key)
         r = supa_api.sign_in_with_password(user, password)
 
-        channel_data_response = supa_api.raw_get("youtube_channel_credentials", "*","id=eq."+str(channel_id))
+        channel_data_response = supa_api.raw_get(
+            "youtube_channel_credentials", "*", "id=eq."+str(channel_id))
         if (channel_data_response) is None:
             return None
         channel_data = channel_data_response[0]
@@ -32,7 +33,7 @@ class youtube_client:
 
     def update_video_status(video_data, video_processing_result):
         print("\n\n\n******   update_video_status is running and updating video status to SUPABASE   ******\n")
-        
+
         url: str = os.environ.get("SUPABASE_URL")
         key: str = os.environ.get("SUPABASE_ANON")
         user: str = os.environ.get("SUPABASE_USER")
@@ -40,30 +41,32 @@ class youtube_client:
         supa_api: RestClient = RestClient(url, key)
         r = supa_api.sign_in_with_password(user, password)
 
-        print ('TODO: doplnit json')
+        print('TODO: doplnit json')
         json_data = {
             "youtube_title": video_data['youtube_title'],
             "youtube_description": video_data['youtube_description'],
             "youtube_keywords": video_data['youtube_keywords'],
             "youtube_category": video_data['youtube_category'],
             "supa_server_role_key": video_data['supa_server_role_key']
-	    }
-        response = supa_api.raw_patch("youtube_video", "id=eq."+str(video_data['id']), json_data) 	
+        }
+        response = supa_api.raw_patch(
+            "youtube_video", "id=eq."+str(video_data['id']), json_data)
         print(response)
 
         supa_api.raw_post("youtube_video", '')
         print("\n\n\n******   update_video_status FINISHED   ******\n")
 
     def youtube_upload(video_data):
-        
+
         ############################################
-        # Set up video description, and attributes  
+        # Set up video description, and attributes
         # so the video can be uploaded to youtube
-        ############################################   
-        vide_file_path = file_path=video_dir + video_data['file_cloud_identifier']
+        ############################################
+        vide_file_path = file_path = video_dir + \
+            video_data['file_cloud_identifier']
         if not os.path.exists(vide_file_path):
             exit("FILE DOES NOT EXISTS")
-    
+
         # setting up the video that is going to be uploaded
         video = LocalVideo(vide_file_path)
 
@@ -72,44 +75,43 @@ class youtube_client:
         video.set_description(video_data['youtube_description'])
         video.set_tags(video_data['youtube_keywords'])
         video.set_category(video_data['youtube_category'])
-    
-    
+
         # setting status
         video.set_default_language("en-US")
         video.set_embeddable(True)
         video.set_license("creativeCommon")
         video.set_privacy_status("public")
         video.set_public_stats_viewable(True)
-        video.set_made_for_kids(True)    
-    
+        video.set_made_for_kids(True)
+
         print('TODO: DOROBIT THUMBNAIL MANAGEMENT')
         # TODO: setting thumbnail
-        #video.set_thumbnail_path(thumbnail_dir + "\\"+ 'thumbnail_'+ question_type[1:].lower() +'_quiz_question_heigh.png')
-    
+        # video.set_thumbnail_path(thumbnail_dir + "\\"+ 'thumbnail_'+ question_type[1:].lower() +'_quiz_question_heigh.png')
+
         # loggin into the channel
         channel = Channel()
         channel.login("client_secrets_focus.json", "credentials.storage")
-    
+
         # uploading video and printing the results
         video_response = channel.upload_video(video)
         video_response.like()
-        print('Video was uploaded with following Youtube ID',video_response.id)
+        print('Video was uploaded with following Youtube ID', video_response.id)
         return video_response
-    
+
     def __init__(self, channel_id):
         print("initializing Youtube Uploader")
         load_dotenv()
         global filecloud_dir
         global video_dir
         global channel_data
-        
-        channel_data = self.load_channel_data(channel_id)        
-        filecloud_dir = os.getenv('FILECLOUD_DIR')
-        video_dir = filecloud_dir + channel_data['filecloud_dir']
-        print(video_dir)
 
-        #video_json = '{filename: "1.mp4"}' ### REPLACE FOR INFORMATION FROM SUPABASE DATA
-        #print(channel_credentials)
-        #channel_credentials = '{credentials: "token"}' ### REPLACE FOR INFORMATION FROM SUPABASE DATA
-        #print(video_json)
-        #youtube_upload(video_dir, video_json, channel_credentials)        
+        channel_data = self.load_channel_data(channel_id)
+        filecloud_dir = os.getenv('FILECLOUD_DIR')
+        video_dir = "xxxxxxxxxxxxxxxxxx"
+        # video_dir = filecloud_dir + channel_data['filecloud_dir']
+        print(video_dir)
+        # video_json = '{filename: "1.mp4"}' ### REPLACE FOR INFORMATION FROM SUPABASE DATA
+        # print(channel_credentials)
+        # channel_credentials = '{credentials: "token"}' ### REPLACE FOR INFORMATION FROM SUPABASE DATA
+        # print(video_json)
+        # youtube_upload(video_dir, video_json, channel_credentials
